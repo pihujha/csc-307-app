4f1b1b488448ca11303594483ec1d9357e8873fd
Pihu Jha <72624166+pihujha@users.noreply.github.com>
2025-10-07 20:38:46 -0700

    IA3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,19 +1,63 @@
 // src/MyApp.jsx
-import React, {useState} from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
 const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => i !== index);
-    setCharacters(updated);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+  }, [] );
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
-  } // append basically
+    postUser(person)
+      .then(res => {
+        if (res.status === 201) {
+          return res.json();
+        } else {
+          throw new Error("Failed to create user");
+        }
+      })
+      .then(newUser => setCharacters([...characters, newUser]))
+      .catch(err => console.log(err));
+  }
+
+
+function removeOneCharacter(id) {
+  fetch(`http://localhost:8000/users/${id}`, { method: "DELETE" })
+    .then(res => {
+      if (res.status === 204) {
+        setCharacters(prev => prev.filter(c => c.id !== id));
+      } else if (res.status === 404) {
+        console.log("User not found");
+      } else {
+        console.log("Delete failed with status", res.status);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
 
   return (
     <div className="container">
